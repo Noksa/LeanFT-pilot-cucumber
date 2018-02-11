08f65427ed26c868f676c974a6033a39cafd562e
Alexandr D <doomeeg@gmail.com>
2018-02-11 11:40:47 +0300

    Without TSRX file and thrash from SDK LEANFT

---
+++ src/main/java/core/AppRepository.java
@@ -0,0 +1,42 @@
+package core;
+
+import com.hp.lft.sdk.Desktop;
+import com.hp.lft.sdk.GeneralLeanFtException;
+import com.hp.lft.sdk.wpf.Window;
+import com.hp.lft.sdk.wpf.WindowDescription;
+import com.sun.deploy.xml.GeneralEntity;
+import ru.sbtqa.tag.pagefactory.annotations.ElementTitle;
+import ru.sbtqa.tag.pagefactory.annotations.PageEntry;
+
+@PageEntry(title = "XmlGenerator")
+public class AppRepository {
+
+   public  AppRepository() throws GeneralLeanFtException{}
+
+
+    @ElementTitle("Главное окно")
+    public Window MainWindow = getMainWindow();
+
+    private Window getMainWindow() throws GeneralLeanFtException
+    {
+        if (MainWindow == null) {
+            MainWindow = Desktop.describe(Window.class, new WindowDescription.Builder()
+                    .objectName("Генератор отчётностей")
+                    .fullType("window")
+                    .windowTitleRegExp("Генератор отчётностей").build());
+        }
+        return MainWindow;
+
+    }
+
+    // в этом случае поиск элемента будет осуществляться каждый раз.
+
+//    private Window getMainWindow()  throws GeneralLeanFtException
+//    {
+//            return  Desktop.describe(Window.class, new WindowDescription.Builder()
+//                    .objectName("Генератор отчётностей")
+//                    .fullType("window")
+//                    .windowTitleRegExp("Генератор отчётностей").build());
+//
+//    }
+}
--- src/main/java/stepdefs/CommonSteps.java
+++ src/main/java/stepdefs/CommonSteps.java
@@ -3,28 +3,18 @@ package stepdefs;
 import com.hp.lft.report.Reporter;
 import com.hp.lft.report.Status;
 import com.hp.lft.sdk.*;
-import com.hp.lft.sdk.stdwin.*;
 import com.hp.lft.sdk.wpf.Window;
-import core.CalcRep;
-import core.Repository;
-import cucumber.api.PendingException;
+import core.AppRepository;
 import cucumber.api.java.*;
 import cucumber.api.java.ru.*;
-import ru.sbtqa.tag.pagefactory.annotations.ActionTitle;
 import ru.sbtqa.tag.pagefactory.annotations.ElementTitle;
 
 import java.net.URI;
-import java.util.EnumSet;
-import java.util.Objects;
 
 public class CommonSteps {
 
-    public CommonSteps() throws GeneralLeanFtException
-    {
-    }
-
-    Repository rep = new Repository();
-
+    public CommonSteps() throws GeneralLeanFtException{}
+    AppRepository appRepository = new AppRepository();
     @Before
     public void test() throws Exception{
         ModifiableSDKConfiguration config = new ModifiableSDKConfiguration();
@@ -41,37 +31,29 @@ public class CommonSteps {
 
 
 
-    @ElementTitle("Окно")
-    public Window MainWindow = rep.Window();
-
-
 
 
     @Дано("^Запустить приложение XmlGenerator$")
     public void открытьПриложениеXmlGenerator() throws Throwable {
         Reporter.startTest("Test app XmlGenerator");
-        if (MainWindow.exists(1)) MainWindow.close();
+        if (appRepository.MainWindow.exists(0)) appRepository.MainWindow.close();
         new ProcessBuilder("C:\\XmlGenerator.exe").start();
         Reporter.setReportTitle("Status Reporting");
-
     }
 
     @Когда("^открывается приложение XmlGenerator$")
     public void открываетсяПриложениеXmlGenerator() throws Throwable {
-        if (MainWindow.exists(60))
-        {
-            Reporter.reportEvent(MainWindow.getDisplayName() + " is opened.", "", Status.Passed);
-        }
+        if (appRepository.MainWindow.exists(60)) Reporter.reportEvent("App is opened", "", Status.Passed);
     }
 
     @Тогда("^пользователь закрывает его$")
     public void пользовательЗакрываетЕго() throws Throwable {
-        MainWindow.close();
+        appRepository.MainWindow.close();
     }
 
     @Тогда("^приложение XmlGenerator закрывается$")
     public void приложениеXmlGeneratorЗакрывается() throws Throwable {
-        notExists(MainWindow);
+        if (notExists(appRepository.MainWindow)) Reporter.reportEvent("App is closed", "", Status.Passed);
     }
 
 
